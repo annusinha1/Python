3733853e1bff9ee63943db64715f9857e218afe4
annusinha1 <annusinha15029@gmail.com>
2024-06-06 11:54:15 +0530

    character taken by user

--- character.py
+++ character.py
@@ -1,4 +1,9 @@
-a = "Abc"
-b = "Def"
-sum = a + b
-print ("The letter is: ", sum)
+# a = "Abc"
+# b = "Def"
+# sum = a + b
+# print ("The letter is: ", sum)
+
+a = input("Enter a fname: ")
+b = input("Enter a lname: ")
+sum = a+b
+print (" The Name is: ", sum)
