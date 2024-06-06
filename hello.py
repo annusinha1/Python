1f14e83cf47532eba85e1077ae2e11e78f690968
annusinha1 <annusinha15029@gmail.com>
2024-06-06 11:47:09 +0530

    Intro of Python

---
+++ hello.py
@@ -0,0 +1,26 @@
+# a = 20
+# b = 30
+# sum = a + b
+# print (" sum is: ", sum)
+# sub = a - b
+# print (" sub is: ", sub)
+# mul = a * b
+# print (" mul is:", mul)
+# div = a / b
+# print (" div is:", div)
+
+# a = input("Enter a fname : ")
+# b = input("Enter a lname : ")
+# sum = a + b
+# print ("The name is : ", sum)
+
+a = int(input("Enter a no: "))
+b = int (input("Enter a no: "))
+sum = a + b
+print("Sum is: ", sum)
+sub = a - b
+print (" sub is: ", sub)
+mul = a * b
+print ("Mul is: ", mul)
+div = a / b
+print (" div is: ", div)
